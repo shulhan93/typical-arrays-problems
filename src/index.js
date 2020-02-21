765557eac7e35c556dfb2bc4b438815d0c965935
Shulhan Andrei <shulhan.andrei@gmail.com>
2020-02-21 19:59:27 +0300

    feat: finished task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
+exports.min = function min(array = 0) {
+    if ((array.length == 0) || (array == 0)) {
+        return 0
+    }
 
-exports.min = function min (array) {
-  return 0;
+    return array.sort((a, b) => a - b)[0]
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array = 0) {
+    if ((array.length == 0) || (array == 0)) {
+        return 0
+    }
+
+    return array.sort((a, b) => b - a)[0]
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array = 0) {
+    if ((array.length == 0) || (array == 0)) {
+        return 0
+    }
+    let sum = array.reduce((sum, cur) => sum + cur);
+    let avg = Math.floor((sum / array.length) * 100) / 100
+    return avg
 }
